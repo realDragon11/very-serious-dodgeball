dba36cbdfb1eb07d61a628c8f3f06a77f5bd68af
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-05-07 12:18:43 -0400

    work on foes

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -6,30 +6,53 @@ using UnityEngine.AI;
 public class EnemyAi : MonoBehaviour
 {
     public enum State{
-        SEEK
+        SEEK,BALL_SEEK, FLEE
     };
     private State state = State.SEEK;
     private Vector3 targetPos;
     private float followDis = 3f;
     public NavMeshAgent agent;
     private GameObject player;
+    public EnemyManager manager;
+    private GameObject ballSeeking;
+    private int ticker = Random.Range(1,10);
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        manager.add(this);
     }
 
     // Update is called once per frame
     void Update()
     {
+        ticker++;
+        if (ticker > 15){
         targetPos = transform.position;
+        ballSeeking = null;
         switch (state){
             case State.SEEK:
                 if (Mathf.Abs((player.transform.position - this.gameObject.transform.position).magnitude) > followDis){
                     targetPos = player.gameObject.transform.position;
                 }
             break;
+            case State.BALL_SEEK:
+
+            if (ballSeeking = null){
+            ballSeeking = manager.requestBall(this);
+            }
+            if (ballSeeking != null){
+                targetPos = ballSeeking.transform.position;
+            }else{
+                state = State.FLEE;
+            }
+            
+            break;
+            case State.FLEE:
+
+            break;
         }
         agent.destination = targetPos;
+        }
     }
 }
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void add(EnemyAi foe){
+        
+    }
+
+    public GameObject requestBall(EnemyAi foe){
+        return null;
+    }
+}
